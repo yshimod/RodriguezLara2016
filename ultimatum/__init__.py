8bd612d7ecbdfb04fa182a39b574965c51dfc254
yshimod <55014385+yshimod@users.noreply.github.com>
2022-07-07 00:52:42 +0900

    7月7日の目標．

--- ultimatum/__init__.py
+++ ultimatum/__init__.py
@@ -2,7 +2,7 @@ from otree.api import *
 import random
 
 doc = """
-Your app description
+main task
 """
 
 
@@ -17,9 +17,17 @@ class Subsession(BaseSubsession):
 
 
 def creating_session(subsession: Subsession):
+    subsession.group_randomly()
+
+    tmporder = ["ult", "nvc"]
+    if not subsession.session.config["ult_is_first"]:
+        tmporder.reverse()
+    subsession.session.vars["scenario_order"] = tmporder
+
     for group in subsession.get_groups():
         group.a_p = 150
         group.a_r = random.choice([100, 150, 200])
+        group.realized = random.choice(tmporder)
 
 
 class Group(BaseGroup):
@@ -37,16 +45,35 @@ class Group(BaseGroup):
     pie_tot = models.FloatField()
 
     # 意思決定
-    offer_p = models.FloatField()
-    offer_r = models.FloatField()
+    offer_p_ult = models.FloatField()
+    offer_r_ult = models.FloatField()
+    offer_p_nvc = models.FloatField()
+    offer_r_nvc = models.FloatField()
 
     # 結果
-    accepted = models.BooleanField()
+    accepted_ult = models.BooleanField()
+    accepted_nvc = models.BooleanField()
+
+    # 支払対象のゲーム
+    realized = models.StringField()
 
 
 class Player(BasePlayer):
-    payoff_ultimatum = models.FloatField()
+    payoff_ult = models.FloatField()
+    payoff_nvc = models.FloatField()
 
+
+def offer_p_ult_max(group: Group):
+    return group.pie_tot
+
+def offer_r_ult_max(group: Group):
+    return group.pie_tot
+
+def offer_p_nvc_max(group: Group):
+    return group.pie_tot
+
+def offer_r_nvc_max(group: Group):
+    return group.pie_tot
 
 
 # PAGES
@@ -77,32 +104,174 @@ class InstructionWaitPage(WaitPage):
     pass
 
 
-class Proposer(Page):
+class Proposer1(Page):
+    template_name = __name__ + "/decision.html"
     form_model = "group"
-    form_fields = ["offer_p"]
 
     @staticmethod
-    def is_displayed(player):
+    def is_displayed(player: Player):
         return player.id_in_group == 1
 
-
-class Responder(Page):
-    form_model = "group"
-    form_fields = ["offer_r"]
+    @staticmethod
+    def get_form_fields(player: Player):
+        flag = player.session.vars["scenario_order"][0]
+        return ["offer_p_{}".format(flag)]
 
     @staticmethod
-    def is_displayed(player):
+    def vars_for_template(player: Player):
+        flag = player.session.vars["scenario_order"][0]
+        return dict(
+            scenario = flag,
+            scenarionum = 1,
+            role = "proposer"
+        )
+
+
+class Responder1(Page):
+    template_name = __name__ + "/decision.html"
+    form_model = "group"
+
+    @staticmethod
+    def is_displayed(player: Player):
         return player.id_in_group == 2
+
+    @staticmethod
+    def get_form_fields(player: Player):
+        flag = player.session.vars["scenario_order"][0]
+        return ["offer_r_{}".format(flag)]
+
+    @staticmethod
+    def vars_for_template(player: Player):
+        flag = player.session.vars["scenario_order"][0]
+        return dict(
+            scenario = flag,
+            scenarionum = 1,
+            role = "responder"
+        )
+
+
+class ScenarioWaitPage(WaitPage):
+    pass
+
+
+class Proposer2(Page):
+    template_name = __name__ + "/decision.html"
+    form_model = "group"
+
+    @staticmethod
+    def is_displayed(player: Player):
+        return player.id_in_group == 1
+
+    @staticmethod
+    def get_form_fields(player: Player):
+        flag = player.session.vars["scenario_order"][1]
+        return ["offer_p_{}".format(flag)]
+
+    @staticmethod
+    def vars_for_template(player: Player):
+        flag = player.session.vars["scenario_order"][1]
+        return dict(
+            scenario = flag,
+            scenarionum = 2,
+            role = "proposer"
+        )
+
+
+class Responder2(Page):
+    template_name = __name__ + "/decision.html"
+    form_model = "group"
+
+    @staticmethod
+    def is_displayed(player: Player):
+        return player.id_in_group == 2
+
+    @staticmethod
+    def get_form_fields(player: Player):
+        flag = player.session.vars["scenario_order"][1]
+        return ["offer_r_{}".format(flag)]
+
+    @staticmethod
+    def vars_for_template(player: Player):
+        flag = player.session.vars["scenario_order"][1]
+        return dict(
+            scenario = flag,
+            scenarionum = 2,
+            role = "responder"
+        )
 
 
 class ResultsWaitPage(WaitPage):
     @staticmethod
     def after_all_players_arrive(group: Group):
-        # ここで利得を計算する．
-        pass
+        """
+        利得を計算する
+        """
+        proposer: Player = group.get_player_by_id(1)
+        responder: Player = group.get_player_by_id(2)
+
+        ## ultimatum game
+        if group.offer_p_ult >= group.offer_r_ult:
+            group.accepted_ult = True
+            proposer.payoff_ult = group.pie_tot - group.offer_p_ult
+            responder.payoff_ult = group.offer_p_ult
+        else:
+            group.accepted_ult = False
+            proposer.payoff_ult = 0
+            responder.payoff_ult = 0
+
+        ## no-veto-cost game
+        if group.offer_p_nvc >= group.offer_r_nvc:
+            group.accepted_nvc = True
+            proposer.payoff_nvc = group.pie_tot - group.offer_p_nvc
+            responder.payoff_nvc = group.offer_p_nvc
+        else:
+            group.accepted_nvc = False
+            proposer.payoff_nvc = group.pie_tot - group.offer_r_nvc
+            responder.payoff_nvc = group.offer_r_nvc
+
+        ## realized
+        if group.realized == "ult":
+            proposer.payoff = proposer.payoff_ult
+            responder.payoff = responder.payoff_ult
+        else:
+            proposer.payoff = proposer.payoff_nvc
+            responder.payoff = responder.payoff_nvc
+
 
 class Results(Page):
-    pass
+    @staticmethod
+    def vars_for_template(player: Player):
+        group: Group = player.group
+
+        results_ult = dict(
+            offer_p = group.offer_p_ult,
+            offer_r = group.offer_r_ult,
+            accepted = group.accepted_ult,
+            payoff_tmp = player.payoff_ult
+        )
+        results_nvc = dict(
+            offer_p = group.offer_p_nvc,
+            offer_r = group.offer_r_nvc,
+            accepted = group.accepted_nvc,
+            payoff_tmp = player.payoff_nvc
+        )
+
+        results = [results_ult, results_nvc]
+
+        firstgame =  player.session.vars["scenario_order"][0]
+
+        if firstgame == "nvc":
+            results.reverse()
+
+        if group.realized == firstgame:
+            chosen_scenario = 1
+        else:
+            chosen_scenario = 2
+
+        return dict(
+            results = results,
+            chosen_scenario = chosen_scenario
+        )
 
 
 page_sequence = [
@@ -110,8 +279,11 @@ page_sequence = [
     Info,
     Instruction,
     InstructionWaitPage,
-    Proposer,
-    Responder,
+    Proposer1,
+    Responder1,
+    ScenarioWaitPage,
+    Proposer2,
+    Responder2,
     ResultsWaitPage,
     Results
 ]
